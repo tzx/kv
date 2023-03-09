7654f62e4d58f26cece5bb32bd931cdd2df8846d
Timmy Xiao <34635512+tzx@users.noreply.github.com>
2023-03-09 17:46:23 -0500

    client + server communication

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,12 @@
+use std::{net::TcpStream, io::{Write, Read}};
+use std::str;
+
 fn main() {
     println!("hello world, i'm a client");
+    let mut stream = TcpStream::connect("127.0.0.1:1234").expect("failed to connect");
+
+    let _bytes_written = stream.write("hello".as_bytes()).expect("Failed to write to server");
+    let mut read_buffer = [0u8; 64];
+    let _bytes_read = stream.read(&mut read_buffer).expect("Failed to read from server");
+    println!("server says: {}", str::from_utf8(&read_buffer).unwrap());
 }
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,12 +1,16 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::str;
 
-fn do_something(connection: TcpStream) {
+fn do_something(mut connection: TcpStream) {
+    let mut read_buffer = [0u8; 64];
+    let _n = connection.read(&mut read_buffer).expect("Failed to read");
+    println!("Client says: {}", str::from_utf8(&read_buffer).unwrap());
+    connection.write("world".as_bytes()).expect("Failed to write");
 }
 
 fn main() {
     println!("hello i'm a server!");
-
-    let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:1234").expect("Failed to bind");
     for incoming in listener.incoming() {
         let stream = incoming.unwrap();
         do_something(stream);
